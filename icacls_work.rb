4f23d313d4706be3081e43aa3a9f18dcb829e938
OlhaBahno <olya.bagno@gmail.com>
2019-04-03 15:32:44 +0300

    Refactor code #2: adding constant, renaming methods

--- icacls_work.rb
+++ icacls_work.rb
@@ -1,12 +1,21 @@
 class Permission
 
   attr_reader :path, :name
+
+  PERMISSIONS = { '(F)' => 'full access',
+                  '(N)' => 'no access',
+                  '(M)' => 'modify access',
+                  '(RX)' => 'read-and-execute access',
+                  '(R)' => 'read access',
+                  '(W)' => 'write access',
+                  '(D)' => 'delete access' }.freeze
+
   def initialize(path, name)
     @path = path
     @name = name
   end
 
-  def users_string
+  def get_users_string
     line = `cd #{@path} && icacls #{@name}`
     line_users = ''
     (line.index('Users')..line.size).each do |number|
@@ -18,14 +27,7 @@ class Permission
 
   def print_permissions(permission_line)
     result_string = 'Users have '
-    permission = { '(F)' => 'full access',
-                   '(N)' => 'no access',
-                   '(M)' => 'modify access',
-                   '(RX)' => 'read-and-execute access',
-                   '(R)' => 'read access',
-                   '(W)' => 'write access',
-                   '(D)' => 'delete access' }
-    permission.each do |number, value|
+    PERMISSIONS.each do |number, value|
       result_string += value if permission_line.include?number
     end
     result_string
