fa394919c6c3da6373318570e498a47590a870a5
Olha Bahno <olya.bagno@gmail.com>
2019-04-10 15:52:22 +0300

    Delete final_result_test.rb

--- icacls_files/final_result_test.rb
+++
@@ -1,6 +0,0 @@
-require './icacls_files/icacls_work.rb'
-module ResultTesting
-  def final_result(user_permission)
-    user_permission.print_permissions(user_permission.get_users_string)
-  end
-end
